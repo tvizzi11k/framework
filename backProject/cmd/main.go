9bdce74f43bc9b36baedd0482a28c1d01dc9f92c
unknown <temp@STUD.UNV.SU>
2024-03-19 11:27:36 +0300

    fix comments

--- backProject/cmd/main.go
+++ backProject/cmd/main.go
@@ -1,145 +1,137 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"html/template"
-	"log"
-	"net/http"
+ "database/sql"
+ "fmt"
+ "html/template"
+ "log"
+ "net/http"
 
-	_ "github.com/go-sql-driver/mysql"
+ _ "github.com/go-sql-driver/mysql"
 )
 
 type Controller interface {
-	HandleRequest(w http.ResponseWriter, req *http.Request)
+ HandleRequest(w http.ResponseWriter, req *http.Request)
 }
 
 type Model interface {
-	Save(db Database) error
+ Save(db Database) error
 }
 
 type Router struct {
-	routes map[string]Controller
+ routes map[string]Controller
 }
 
 func (r *Router) HandleFunc(path string, controller Controller) {
-	if r.routes == nil {
-		r.routes = make(map[string]Controller)
-	}
-	r.routes[path] = controller
+ if r.routes == nil {
+  r.routes = make(map[string]Controller)
+ }
+ r.routes[path] = controller
 }
 
 func (r *Router) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	path := req.URL.Path
-	if controller, ok := r.routes[path]; ok {
-		controller.HandleRequest(w, req)
-	} else {
-		http.NotFound(w, req)
-	}
+ path := req.URL.Path
+ if controller, ok := r.routes[path]; ok {
+  controller.HandleRequest(w, req)
+ } else {
+  http.NotFound(w, req)
+ }
 }
 
 func NewRouter() *Router {
-	return &Router{
-		routes: make(map[string]Controller),
-	}
-}
-
-type User struct {
-	ID       int
-	Username string
-	Email    string
+ return &Router{
+  routes: make(map[string]Controller),
+ }
 }
 
 type Database interface {
-	Query(query string, args ...interface{}) (*sql.Rows, error)
-	Exec(query string, args ...interface{}) (sql.Result, error)
+ Query(query string, args ...interface{}) (*sql.Rows, error)
+ Exec(query string, args ...interface{}) (sql.Result, error)
 }
 
 func (u *User) Save(db Database) error {
-	result, err := db.Exec("INSERT INTO users (username, email) VALUES (?, ?)", u.Username, u.Email)
-	if err != nil {
-		return err
-	}
-	lastInsertID, _ := result.LastInsertId()
-	u.ID = int(lastInsertID)
-	return nil
+ result, err := db.Exec(fmt.Sprintf("INSERT INTO %s (username, email) VALUES (?, ?)"))
+ if err != nil {
+  return err
+ }
+ lastInsertID, _ := result.LastInsertId()
+ u.ID = int(lastInsertID)
+ return nil
 }
 
-type UserController struct {
-	DB   Database
-	View View
+type GenericController struct {
+ DB   Database
 }
 
 func (uc *UserController) HandleRequest(w http.ResponseWriter, req *http.Request) {
-	username := req.FormValue("username")
-	email := req.FormValue("email")
+ table := req.FormValue("table")
+ columns := req.FormValue("columns")
+ values := req.FormValue("values")
 
-	newUser := &User{
-		Username: username,
-		Email:    email,
-	}
-	if err := newUser.Save(uc.DB); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+ 
+ query := fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", table, columns, values)
 
-	err := uc.View.Render(w, newUser)
-	if err != nil {
-		http.Error(w, "Error rendering HTML template", http.StatusInternalServerError)
-	}
+ result, err := gc.DB.Exec(query)
+ if err != nil {
+  http.Error(w, err.Error(), http.StatusInternalServerError)
+  return
+ }
+
+ fmt.Fprintf(w, "Data inserted")
 }
 
 type View interface {
-	Render(w http.ResponseWriter, data interface{}) error
+ Render(w http.ResponseWriter, data interface{}) error
 }
 
 type HTMLView struct {
-	Template *template.Template
+ Template *template.Template
 }
 
 func (hv *HTMLView) Render(w http.ResponseWriter, data interface{}) error {
-	err := hv.Template.Execute(w, data)
-	if err != nil {
-		return err
-	}
-	return nil
+ err := hv.Template.Execute(w, data)
+ if err != nil {
+  return err
+ }
+ return nil
 }
 
 func NewHTMLView() *HTMLView {
-	tmpl, err := template.ParseFiles("static/templates/index.html")
-	if err != nil {
-		log.Fatal("Error parsing HTML template:", err)
-	}
-	return &HTMLView{
-		Template: tmpl,
-	}
+ tmpl, err := template.ParseFiles("static/templates/index.html")
+ if err != nil {
+  log.Fatal("Error parsing HTML template:", err)
+ }
+ return &HTMLView{
+  Template: tmpl,
+ }
 }
 
 func main() {
-	r := NewRouter()
+ r := NewRouter()
 
-	htmlView := NewHTMLView()
+ htmlView := NewHTMLView()
 
-	db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/shedule")
-	if err != nil {
-		log.Fatal("Error connecting to database:", err)
-	}
+ db, err := sql.Open("mysql", "root:password@tcp(127.0.0.1:3306)/shedule")
+ if err != nil {
+  log.Fatal("Error connecting to database:", err)
+ }
 
-	userController := &UserController{
-		DB:   db,
-		View: htmlView,
-	}
+ userController := &UserController{
+  DB:   db,
+  View: htmlView,
+ }
 
-	r.HandleFunc("/myAddress", userController)
+ r.HandleFunc("/myAddress", userController)
 
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: r,
-	}
+ server := &http.Server{
+  Addr:    ":8080",
+  Handler: r,
+ }
 
-	fmt.Println("Starting server on port 8080")
-	err = server.ListenAndServe()
-	if err != nil {
-		log.Fatal("Server error", err)
-	}
+ fmt.Println("Starting server on port 8080")
+ err = server.ListenAndServe()
+ if err != nil {
+  log.Fatal("Server error", err)
+ }
 }
+
